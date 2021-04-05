960bc592f792c745ea1b47c3b5919868b0139d77
pllorca <p.llorca.pl@gmail.com>
2021-04-05 20:10:16 +0200

    crud product admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,7 +8,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'admin')]
+    /**
+     * @Route("/admin", name="admin")
+     */
     public function index(): Response
     {
         return $this->render('admin/index.html.twig', [
