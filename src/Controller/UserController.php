72d81852984e67ffa18eb46d750162ccd4b0d6df
sabrina-ra <sbr.rabhi25@gmail.com>
2021-04-06 15:13:55 +0200

    sab_admin/user_form

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Form\UserType;
 use App\Controller\UserController;
 use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,4 +20,40 @@ class UserController extends AbstractController
             'customers' => $customers,
         ]);
     }
+    
+    // /**
+    //  * @Route("/admin/users/-{id}", name="admin_users_id")
+    //  */
+    // public function showOneCustomer(UserRepository $userRepository, $id, Request $request): Response
+    // {
+    //     $customer = $UserRepository->find($id);
+    //     return $this->render("admin/users.html.twig" . $id, [
+    //         'customer' => $customer,
+    //     ]);
+    // }
+
+    /**
+     * @Route ("/admin/users/update-{id}", name="admin_user_update")
+     */
+    public function updateCustomer (UserRepository $userRepository, $id, Request $request)
+    {
+        $customer = $userRepository->find($id);
+        $form = $this->createForm(UserType::class, $customer);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($customer);
+            $manager->flush();
+
+            $this->addFlash('success', 'Le User a été bie modifié');
+            return $this->redirectToRoute('admin_users');
+
+        }
+        return $this->render('admin/userForm.html.twig', [
+            'userForm' => $form->createView()
+        ]);
+    }
 }
+
+
