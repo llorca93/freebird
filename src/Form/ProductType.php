08be7062ae6d7425f27b128e914095218bc6eeb3
pllorca <p.llorca.pl@gmail.com>
2021-04-06 10:34:47 +0200

    images para

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -3,12 +3,15 @@
 namespace App\Form;
 
 use App\Entity\Product;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Validator\Constraints\Image;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class ProductType extends AbstractType
@@ -88,7 +91,11 @@ class ProductType extends AbstractType
                     'placeholder' => 'La description du produit'
                 ]
             ])
-            ->add('category')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name'
+            ])
+            ->add('ok', SubmitType::class)
         ;
     }
 
