26f37dbe04e0bc9456c667eef158cda1492908ef
pllorca <p.llorca.pl@gmail.com>
2021-04-07 09:12:21 +0200

    orders_admin_pierre

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -24,15 +24,41 @@ class OrderController extends AbstractController
     }
 
     /**
-     * @Route("/admin/order/create", name="admin_order_create")
+     * @Route("/admin/orders/update-{id}", name="admin_orders_update")
      */
-    public function createOrder(Request $request)
+    public function updateOrder(OrderRepository $orderRepository, $id, Request $request)
     {
-        $order = new Order();
+        $order = $orderRepository->find($id);
         $form = $this->createForm(OrderType::class, $order);
         $form->handleRequest($request);
 
-        
+        if ($form->isSubmitted && $form->isValid) {
+            
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($order);
+            $manager->flush();
+            $this->addFlash('success','La commande a bien été modifiée');
+            return $this->redirectToRoute('admin_product');
+        }
+
+        return $this->render('admin/orderForm.html.twig', [
+            'orderForm' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/orders/delete-{id}", name="admin_orders_delete")
+     */
+    public function deleteOrder(OrderRepository $orderRepository, $id)
+    {
+        $order = $orderRepository->find($id);
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($order);
+        $manager->flush();
+
+        $this->addFlash('success','Le produit a bien été supprimé');
+
+        return $this->redirectToRoute('admin_orders');
 
     }
 }
