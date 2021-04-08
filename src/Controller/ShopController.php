2c689aabf416a5da1d2ef1cc9c7fd648d5bcd9f2
alexombre <moutapamalexandre@gmail.com>
2021-04-08 21:11:55 +0200

    pcontroller

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -5,16 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
+
 
 class ShopController extends AbstractController
 {
     /**
      * @Route("/shop", name="shop")
      */
-    public function index(): Response
-    {
+    public function index(CategoryRepository $categoryRepository,ProductRepository $productRepository): Response
+    {   
+        $categories = $categoryRepository->findAll();
+        $products = $productRepository->findAll();
         return $this->render('shop/index.html.twig', [
             'controller_name' => 'ShopController',
+            'categories' => $categories,
+            'products' => $products,
         ]);
     }
 }
