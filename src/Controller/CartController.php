c925a8e5617fafb07a3af9b1c1fc89ad5f766e8f
pllorca <p.llorca.pl@gmail.com>
2021-04-08 17:57:28 +0200

    ajout_panier_pll

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -10,26 +11,50 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class CartController extends AbstractController
 {
     /**
-     * @Route("/cart", name="cart")
+     * @Route("/cart", name="cart_index")
      */
-    public function index(): Response
+    public function index(SessionInterface $session, ProductRepository $productRepository): Response
     {
-        $id = app.request.query.id
-        $products = app.session.get('products');
-        if ($id != null) {
-            array_push($products, $id);
-            app.session.set('products', array_unique($products));
+        $panier = $session->get('panier', []);
+
+        $panierWidthData = [];
+
+        foreach($panier as $id => $quantity) {
+            $panierWidthData[] = [
+                'product' => $productRepository->find($id),
+                'quantity' => $quantity
+            ]; 
         }
+
+        $total = 0;
+
+        foreach($panierWidthData as $item) {
+            $totalItem = $item['product']->getPrice() * $item['quantity'];
+            $total += $totalItem;
+        }
+        
         return $this->render('cart/index.html.twig', [
-            'products' => $products,
+            'items' => $panierWidthData,
+            'total' => $total
         ]);
     }
 
     /**
-     * @Route("/cart/add-{id}", name="cart_add")
+     * @Route("/cart/add/{id}", name="cart_add")
      */
     public function add($id, SessionInterface $session)
     {
-        
+        $panier = $session->get('panier', []);
+
+        if (!empty($panier[$id])) {
+            $panier[$id]++;
+        } else {
+            $panier[$id] = 1;
+        }
+
+
+        $session->set('panier', $panier);
+
+        return $this->redirectToRoute('cart_index');
     }
 }
