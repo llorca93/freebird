30dfcb835d5a0921ccf81d817bb2398064865e83
sabrina-ra <sbr.rabhi25@gmail.com>
2021-04-13 15:14:43 +0200

    header-replacement-in-baseClient

--- public/index.php
+++ public/index.php
@@ -7,13 +7,13 @@ use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-//
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 
-if ($_SERVER['APP_ENV'] != 'dev') {
+// if ($_SERVER['APP_ENV']) {
 
-} else {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
+// } else {
+//     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+// }
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
