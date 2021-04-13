36f773f69d0f4d79dc79a608fb79bb75f4c2a26b
alexombre <moutapamalexandre@gmail.com>
2021-04-13 09:19:27 +0200

    test deploiement

--- migrations/Version20210406194046.php
+++ migrations/Version20210406194046.php
@@ -28,4 +28,9 @@ final class Version20210406194046 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user DROP is_verified, CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`');
     }
+
+    public function isTransactional(): bool
+{
+    return false;
+}
 }
