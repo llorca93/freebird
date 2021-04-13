e6492944e63323d0daff624d07df5132d80838a8
alexombre <moutapamalexandre@gmail.com>
2021-04-13 09:57:45 +0200

    fix conflict

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -8,14 +8,10 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ClientController extends AbstractController
 {
-<<<<<<< HEAD
-    #[Route('/compte', name: 'compte')]
-=======
     /**
      * @Route("/client", name="client")
      */
     #[Route('/client', name: 'client')]
->>>>>>> 835a038595ad36b39e0d4b7d2dc456037a3a4456
     public function index(): Response
     {
         return $this->render('client/index.html.twig', [
