f38e120c83a2b64cfd99693f061c1cd89bef9e00
sabrina-ra <sbr.rabhi25@gmail.com>
2021-04-14 12:04:02 +0200

    fix-account-path:client-to-compte

--- src/Security/Authenticator.php
+++ src/Security/Authenticator.php
@@ -98,7 +98,7 @@ class Authenticator extends AbstractFormLoginAuthenticator implements PasswordAu
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
-        return new RedirectResponse($this->urlGenerator->generate('client'));
+        return new RedirectResponse($this->urlGenerator->generate('compte'));
     }
 
     protected function getLoginUrl()
